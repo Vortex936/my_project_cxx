3708808bb984a86d27f76ba0f9bfe2529eff9b7b
sigurd4 <sigurd.spangelo@gmail.com>
2025-03-08 16:11:48 +0100

    Fix ninja?

--- build/debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sigurd/Code/c++/programmering2/my_project_cxx
-# Build directory: /home/sigurd/Code/c++/programmering2/my_project_cxx/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[it_works]=] "/home/sigurd/Code/c++/programmering2/my_project_cxx/build/debug/bin/tests" "it_works")
-set_tests_properties([=[it_works]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/sigurd/Code/c++/programmering2/my_project_cxx/CMakeLists.txt;409;add_test;/home/sigurd/Code/c++/programmering2/my_project_cxx/CMakeLists.txt;0;")
