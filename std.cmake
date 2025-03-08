3463a2fb8619ed9af9e257e006563b09f181155f
sigurd4 <sigurd.spangelo@gmail.com>
2025-03-08 17:53:42 +0100

    Re-add rpath

--- std.cmake
+++ std.cmake
@@ -6,10 +6,14 @@ if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "18.1.0")
   message(FATAL_ERROR "std module requires Clang 18.1.0 or later")
 endif()
 
+#execute_process(COMMAND bash "-c" "${CMAKE_CXX_COMPILER} -v 2>&1 > /dev/null | grep 'InstalledDir' | awk '{print $2}' | tr -d '\n'" OUTPUT_VARIABLE CLANG_DIR)
+#execute_process(COMMAND bash "-c" "${CMAKE_CXX_COMPILER} -v 2>&1 > /dev/null | grep 'Target: ' | awk '{print $2}' | tr -d '\n'" OUTPUT_VARIABLE CLANG_TARGET)
+
 include(FetchContent)
 FetchContent_Declare(
   std_module
   URL "file://${STD_LIB_PATH}"
+  #URL "file://${CLANG_DIR}/../share/libc++/v1"
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
   SYSTEM
 )
@@ -39,6 +43,7 @@ target_compile_features(std
 target_link_options(std
   PUBLIC
     -stdlib=libc++
+    -Wl,-rpath,"${STD_LIB_PATH}"
 )
 
 target_link_libraries(std
